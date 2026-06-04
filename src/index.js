06efd133460b0147535b0d40a0cbfa6bb331f04a
Abdul Basit <basithafeez2346@gmail.com>
2026-06-04 18:17:29 +0500

    util code

--- src/index.js
+++ src/index.js
@@ -6,3 +6,9 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((error)=> console.log("MondoDb connection failed! ",error))
