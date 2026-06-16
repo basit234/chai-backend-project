fc75c8ea2de4302ff43693e7be3b6c7f34c14002
Abdul Basit <basithafeez2346@gmail.com>
2026-06-16 20:17:26 +0500

    user registration controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,97 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
+import { asyncHandler } from '../utils/asyncHandler.js';
+import { ApiError } from '../utils/apiError.js';
+import { User } from '../models/user.model.js';
+import { uploadOnCloudinary } from '../utils/cloudinary.js';
+import { ApiResponse } from '../utils/apiResponse.js';
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "hello world",
-     });
+    // get user details from request body
+    // validation - not empty
+    // check if user already exists
+    // check for images , check for avatar
+    // upload images to cloudinary
+    // create user object and save to database
+    // remove password and refresh token from response
+    // check for user creation success
+    // send response
+
+    // get user details from request body
+    const { userName, email, fullName, password } = req.body;
+    console.log(userName, email, fullName, password);
+
+    // validation - not empty
+    const requiredFields = [userName, email, fullName, password];
+
+    if (requiredFields.some((field) => field?.trim() === '')) {
+        throw new ApiError(400, 'All fields are required');
+    }
+
+    // check if user already exists
+    const existedUser = await User.findOne({
+        $or: [{ userName }, { email }],
+    });
+
+    if (existedUser) {
+        throw new ApiError(409, 'User already exists');
+    }
+
+    console.log(req.files);
+
+    // check for images , check for avatar
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (
+        req.files &&
+        Array.isArray(req.files.coverImage) &&
+        req.files.coverImage.length > 0
+    ) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, 'Avatar image is required');
+    }
+
+    // upload imgaes to cloudnary
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+    if (!avatar) {
+        throw new ApiError(400, 'Avatar image upload failed');
+    }
+
+    // create user object and save to database
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || '',
+        email,
+        password,
+        userName: userName.toLowerCase(),
+    });
+
+    // remove password and refresh token from response
+
+    const createdUser = await User.findById(user._id).select(
+        '-password -refreshToken'
+    );
+
+    if (!createdUser) {
+        throw new ApiError(
+            500,
+            'Something went wrong while creating the user.'
+        );
+    }
+
+    //send response
+
+    return res
+        .status(201)
+        .json(
+            new ApiResponse(200, 'User registered successfully', createdUser)
+        );
 });
 
 export { registerUser };
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -4,7 +4,7 @@ import fs from 'fs';
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: CLOUDINARY_API_SECRET,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -14,7 +14,8 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: 'auto',
         });
         // file has been uploaded successfully
-        console.log('File uploaded successfully', response.url);
+        // console.log('File uploaded successfully', response.url);
+        fs.unlinkSync(localFilePath);
         return response;
     } catch (error) {
         fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload failed
@@ -22,4 +23,4 @@ const uploadOnCloudinary = async (localFilePath) => {
     }
 };
 
-export default uploadOnCloudinary;
+export  {uploadOnCloudinary};
