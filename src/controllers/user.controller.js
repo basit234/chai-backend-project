8391bb9ae949ab84e9298091d9cb4bc5f575c2cf
Abdul Basit <basithafeez2346@gmail.com>
2026-07-14 15:34:42 +0500

    user login and logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,25 @@ import { User } from '../models/user.model.js';
 import { uploadOnCloudinary } from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/apiResponse.js';
 
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        // save the refresh token to the database
+        user.refreshToken = refreshToken;
+        await user.save({validateBeforeSave: false});
+
+        return { accessToken, refreshToken };
+        
+    } catch (error) {
+        throw new ApiError(500, 'Something went wrong while generating refresh and access tokens');
+    }
+}
+
+// User registration
+
 const registerUser = asyncHandler(async (req, res) => {
     // get user details from request body
     // validation - not empty
@@ -94,4 +113,99 @@ const registerUser = asyncHandler(async (req, res) => {
         );
 });
 
-export { registerUser };
+// User login
+
+const loginUser = asyncHandler(async (req, res) => {
+    // get user details from request body
+    // username or email - validation - not empty
+    // check if user exists
+    // check for password
+    // access and refresh token generate
+    // remove password and refresh token from response
+    // send cookies
+
+
+    // get user details from request body
+
+    const {email, userName , password} = req.body
+
+    // username or email - validation - not empty
+
+    if (!email || !userName) {
+        throw new ApiError(400, 'Email or username is required');
+    }
+
+    // check if user exists
+
+    const user = await User.findOne({
+        $or : [{email} , {userName}]
+    })
+
+    if (!user) {
+        throw new ApiError(404, 'User not found');
+    }
+
+    // check for password
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, 'Invalid user credentials');
+    }
+
+    // access and refresh token generate
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user?._id);
+
+    //  send cookies
+    const loggedInUser = await User.findById(user._id).select(
+        '-password -refreshToken'
+    );
+    
+    const option = {
+        httpOnly: true,
+        secure: true,
+    };
+
+    return res
+    .status(200)
+    .cookie('accessToken', accessToken, option)
+    .cookie('refreshToken', refreshToken, option)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser,
+                accessToken,
+                refreshToken,
+            },
+            'User logged in successfully'
+        )
+    );
+});
+
+// User logout
+
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findOneAndUpdate(
+        req.user._id, 
+        {
+            refreshToken: undefined
+        },
+        {
+            new: true
+        }
+    )
+    const option = {
+        httpOnly: true,
+        secure: true,
+    };
+
+    return res
+    .status(200)
+    .clearCookie('accessToken', option)
+    .clearCookie('refreshToken', option)
+    .json(new ApiResponse(200, {}, 'User logged out successfully'));
+});
+
+export { registerUser, loginUser , logoutUser };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -53,7 +53,7 @@ const userSchema = new Schema(
 userSchema.pre('save', async function () {
     if (!this.isModified('password')) return ;
     this.password = await bcrypt.hash(this.password, 10);
-    // next();
+    // next(); removed it because newer version of mongoose does not require next if the function is async
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
