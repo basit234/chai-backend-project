135cb992783218f0654dc0f59a7a471f53d6d131
Abdul Basit <basithafeez2346@gmail.com>
2026-09-23 22:21:05 +0500

    Video module done

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,7 +1,5 @@
-import mongoose, {isValidObjectId} from "mongoose"
 import { asyncHandler } from "../utils/asyncHandler.js";
 import Video from "../models/video.model.js"
-import {User} from "../models/user.model.js"
 import {ApiError} from "../utils/apiError.js"
 import {ApiResponse} from "../utils/apiResponse.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
@@ -9,6 +7,47 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 const getAllVideos = asyncHandler(async(req, res)=>{
     const {page = 1, limit = 10, query, sortBy, sortType, userId} = req.query
     // TODO: get all videos based on query, sort, pagination
+    // console.log(req.query, "request")
+    
+    const filter = {}
+    if(query){
+        filter.$or=[
+            {
+                title : {$regex : query}
+            },
+            {    
+                description : {$regex : query}
+            }
+        ]
+    }
+    const sort = {}
+    if(sortBy && sortType) {
+        sort[sortBy] = Number(sortType)
+    }
+    const total = await Video.countDocuments({
+            owner : userId,
+            ...filter
+    })
+    const videos = await Video.find({
+        owner : userId,
+        ...filter
+    })
+    .sort(sort)
+    .skip((page - 1) * limit)
+    .limit(limit *1)
+    
+    return res
+    .status(200)
+    // .json(new ApiResponse(200, videos ,  "All videos"))
+    .json({
+        success : true,
+        message : "All videos",
+        data : videos,
+        total,
+        page : Number(page),
+        limit : Number(limit),
+        totalPages : Math.ceil(total / limit)
+    })
 })
 
 // Create a Video
@@ -27,8 +66,8 @@ const publishAVideo = asyncHandler(async(req, res)=>{
         throw new ApiError(400, 'Video already exists');
     }
 
-    const videoLocalPath = req.files?.videoFile[0].path
-    const thumbnailLocalPath = req.files?.thumbnail[0].path
+    const videoLocalPath = req.files?.videoFile?.[0].path
+    const thumbnailLocalPath = req.files?.thumbnail?.[0].path
 
     if(!videoLocalPath || !thumbnailLocalPath) {
         throw new ApiError(400, 'Video and thumbnail are required');
@@ -97,11 +136,13 @@ const updateVideo = asyncHandler(async(req, res)=>{
     const existingThumbnailPath = existingVideo?.thumbnail
     const newVideo = videoLocalPath ? videoLocalPath.url : existingVideoPath
     const newThumbnail = thumbnailLocalPath ? thumbnailLocalPath.url : existingThumbnailPath
+    const newDuration = videoLocalPath ? videoLocalPath.duration : existingVideo?.duration
     const video = await Video.findByIdAndUpdate(videoId, {
         title,
         description,
         videoFile : newVideo,
-        thumbnail : newThumbnail
+        thumbnail : newThumbnail,
+        duration : newDuration
     }, {
         new : true
     })
